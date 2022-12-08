e301ea88615ec41d4aa944b11ec24f1634d70389
Romenildo Silva <romenildo2017@outlook.com>
2022-12-07 21:52:03 -0300

    atualização

---
+++ desafio01-calculadora/src/components/Button/index.js
@@ -0,0 +1,11 @@
+
+const Button = () => {
+    return (
+      <div className="App">
+        Olá React
+      </div>
+    );
+  }
+  
+  export default Button;
+  
---
+++ desafio01-calculadora/src/components/Input/index.js
@@ -0,0 +1,11 @@
+
+const Input = () => {
+    return (
+      <div className="App">
+        Olá React
+      </div>
+    );
+  }
+  
+  export default Input;
+  
