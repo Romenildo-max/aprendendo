5d2f43d1c9ffb74cce21312f12136a586e3f231e
Romenildo Silva <romenildo2017@outlook.com>
2022-12-08 20:30:36 -0300

    atualização

--- desafio01-calculadora/src/components/Input/index.js
+++ desafio01-calculadora/src/components/Input/index.js
@@ -1,9 +1,9 @@
 import { InputContainer } from './styles';
 
-const Input = () => {
+const Input = ({value}) => {
     return (
       <InputContainer>
-        <Input />
+        <Input disabled value={value} />
       </InputContainer>
     );
   }
